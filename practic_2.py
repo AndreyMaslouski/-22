2cdd90bfafbf76359d1a51fcdb82ab686ca87578
AndreyMaslouski <98236819+AndreyMaslouski@users.noreply.github.com>
2022-05-11 09:54:54 +0300

    Add files via upload

---
+++ practic_2.py
@@ -0,0 +1,16 @@
+def season(a):
+    if a==12 or 1<=a<=2:
+        print("Зима")
+    elif   3<=a<=5:
+        print("Весна")
+    elif  6<=a<=8:
+        print("Лето")
+    elif  9<=a<=11:
+        print("Осень")
+
+d = int(input())
+season(d)
+
+
+
+
