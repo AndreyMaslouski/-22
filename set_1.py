65e8568ad6626cb23f0297c1bd7b0371535a95b5
AndreyMaslouski <98236819+AndreyMaslouski@users.noreply.github.com>
2022-05-15 17:48:42 +0300

    Add files via upload

---
+++ set_1.py
@@ -0,0 +1,19 @@
+a = frozenset (['hello','world','sunny'])
+print(type(a))
+
+# b = {1,8,7,6,3,2,2,2,2,2,2,2}
+# print(b)
+
+a.add('red')
+print(a)
+# a.remove(4)
+# a.discard(4)
+# print(a.clear())
+# a = {'hello','world','sunny'}
+# a = 7
+# b = {1,8,7,6,3,2,2,2,2,2,2}
+# c = {1,8,5,2,4,13,16}
+# print(b.isdisjoint(a))
+# print(c-b)
+# print(c.union(a,b))
+# print(len(a))
